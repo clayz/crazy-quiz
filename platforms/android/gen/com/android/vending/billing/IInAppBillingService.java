e245a206c4fdd009342cacd478505099661e03e1
Clay Zhong <zjclay@gmail.com>
2014-06-05 13:12:54 +0900

    Updated file path comment.

--- platforms/android/gen/com/android/vending/billing/IInAppBillingService.java
+++ platforms/android/gen/com/android/vending/billing/IInAppBillingService.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/zhong_jie/Work/CrazyQuiz/crazy-quiz/platforms/android/src/com/android/vending/billing/IInAppBillingService.aidl
+ * Original file: /Users/zhong_jie/Source/crazy-quiz/platforms/android/src/com/android/vending/billing/IInAppBillingService.aidl
  */
 package com.android.vending.billing;
 /**
