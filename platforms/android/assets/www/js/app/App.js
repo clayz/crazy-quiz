62cedbabc683c56fe5864d1a0a88c3e8b9497c30
Clay Zhong <zjclay@gmail.com>
2014-09-02 00:53:17 +0900

    Fixed android click slow issue.

--- platforms/android/assets/www/js/app/App.js
+++ platforms/android/assets/www/js/app/App.js
@@ -51,7 +51,7 @@ CQ.App = {
         $.mobile.defaultDialogTransition = 'none';
         $.mobile.buttonMarkup.hoverDelay = 0;
 
-        if (this.android()) FastClick.attach(document.body);
+        if (CQ.App.android()) FastClick.attach(document.body);
         document.addEventListener('backbutton', CQ.App.back, false);
 
         console.log('App initialization finished.');
