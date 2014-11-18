42059401891840981661fd5a873a176ca8ad2eda
Clay Zhong <zjclay@gmail.com>
2014-11-18 23:29:06 +0900

    Added refresh function for daily bonus popup.

--- platforms/ios/www/js/page/Main.js
+++ platforms/ios/www/js/page/Main.js
@@ -111,6 +111,7 @@ CQ.Page.Main = {
 
         $('#' + this.name).on('pageshow', function() {
             if (CQ.Page.Main.dailyBonus.ifGetBonusToday()) {
+                CQ.Page.Main.dailyBonus.refresh();
                 CQ.Page.openPopup(CQ.Page.Main.dailyBonus);
             } else if (!CQ.Datastore.User.isRated()) {
                 // open rating popup if required
